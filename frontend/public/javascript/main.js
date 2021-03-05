d5c3d93a25f903fd05e2d7ca8034ecb67f75201d
carl <jerritt.aquino@gmail.com>
2021-03-05 16:49:16 -0500

    basic frontend

---
+++ frontend/public/javascript/main.js
@@ -0,0 +1,33 @@
+let posts = [
+  {
+    content: "This is Carl's post",
+    username: "Carl",
+    comments: [
+      {
+        username: 'Graham',
+        comment: 'This is a cool post!'
+      }
+    ]
+  },
+  {
+    content: "This is Graham's post",
+    username: "Graham",
+    comments: [
+      {
+        username: 'Carl',
+        comment: 'This is an even cooler post!'
+      },
+      {
+        username: 'Chris Lee',
+        comment: 'You guys should apply for Summer Capstone ;)'
+      },
+    ]
+  },
+]
+
+let postsTemplate  = Handlebars.compile($('#posts').html());
+
+Handlebars.registerPartial('post', $('#post').html());
+Handlebars.registerPartial('comment', $('#comment').html());
+
+$('#content ul').append(postsTemplate({ posts: posts }));
