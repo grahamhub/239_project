5e5c7b71adbc6f8f429ea938c6e22e21e0f9981c
Graham Robertson <graham@grahamcracker.dev>
2021-03-05 13:44:51 -0600

    create user

--- api.rb
+++ api.rb
@@ -4,10 +4,48 @@ require "sinatra/reloader" if development?
 require "bcrypt"
 require "json"
 
-class User; end
+class User
+  def self.get(user_id)
+    user = DB.get("users", {id: user_id})
+    {
+      user_id: user_id,
+      username: user[:username],
+      bio: user[:bio]
+    }
+  end
+
+  def self.authenticate(params)
+    user = DB.get("users", {id: params[:user_id]})
+    user[:password] == params[:password]
+  end
+  
+  def self.post(params)
+    DB.add("posts", params)
+  end
+
+  def self.comment(params)
+    DB.add("comments", params)
+  end
+
+  def self.remove_post(post_id)
+    DB.remove("posts", post_id)
+  end
+
+  def self.remove_comment(comment_id)
+    DB.remove("comments", comment_id)
+  end
+end
 
 class API < Sinatra::Base
   set :sessions => true
+
+  register do
+    def auth(type)
+      condition do
+        403 unless send("is_#{type}?")
+      end
+    end
+  end
 
   helpers do
     def is_user?
